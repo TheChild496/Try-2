a677eb0b92bea9188ce0cb4a36e1ab334ed0393d
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-20 09:11:20 +0100

    changed: simplified MODID and the usage of it

--- src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kaupenjoe/tutorialmod/block/ModBlocks.java
@@ -34,7 +34,7 @@ public class ModBlocks {
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
-        return Registry.register(Registry.BLOCK, new Identifier(TutorialMod.MOD_ID, name), block);
+        return Registry.register(Registry.BLOCK, new Identifier(MODID, name), block);
     }
 
     private static void registerBlockItem(String name, Block block, ItemGroup tab) {
@@ -43,6 +43,6 @@ public class ModBlocks {
 	}
 
     public static void registerModBlocks() {
-        TutorialMod.LOGGER.debug("Registering ModBlocks for " + TutorialMod.MOD_ID);
+        TutorialMod.LOGGER.debug("Registering ModBlocks for " + MODID);
     }
 }
