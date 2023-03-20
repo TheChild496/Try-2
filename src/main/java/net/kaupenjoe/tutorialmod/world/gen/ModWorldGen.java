b8cc351d4237cbd1ad58aaae7ca4b246a45a9c40
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-20 09:25:35 +0100

    changed: (cosmetics)

--- src/main/java/net/kaupenjoe/tutorialmod/world/gen/ModWorldGen.java
+++ src/main/java/net/kaupenjoe/tutorialmod/world/gen/ModWorldGen.java
@@ -2,7 +2,8 @@ package net.kaupenjoe.tutorialmod.world.gen;
 
 public class ModWorldGen {
     public static void generateWorldGen() {
-        ModOreGeneration.generateOres();
+
+		ModOreGeneration.generateOres();
 
         ModEntitySpawn.addEntitySpawn();
     }
