88cae0b1bc0613ec1c525d4686f5b567c4be3542
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-20 09:16:23 +0100

    fixed: the small 'mixed up' of the registrations and the registration keys ;)

--- src/main/java/net/kaupenjoe/tutorialmod/world/dimensions/ModDimensions.java
+++ src/main/java/net/kaupenjoe/tutorialmod/world/dimensions/ModDimensions.java
@@ -5,17 +5,16 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
 
 import static net.kaupenjoe.tutorialmod.TutorialMod.MODID;
 
 public class ModDimensions {
-    private static final RegistryKey<DimensionOptions> DIMENSION_KEY = RegistryKey.of(Registry.DIMENSION_KEY,
-            new Identifier(TutorialMod.MOD_ID, "kjdim"));
-    public static RegistryKey<World> KJDIM_KEY = RegistryKey.of(Registry.WORLD_KEY, DIMENSION_KEY.getValue());
-    private static final RegistryKey<DimensionType> DIMENSION_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
-            new Identifier(TutorialMod.MOD_ID, "kjdim_type"));
+	public static RegistryKey<World> KJDIM_DIMESION_KEY = RegistryKey.of(Registry.WORLD_KEY,
+			new Identifier(MODID, "kjdim"));
+
+	static final RegistryKey<DimensionType> KJDIM_TYPE_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY,
+			KJDIM_DIMESION_KEY.getValue());
 
 
     public static void register() { TutorialMod.LOGGER.debug("Registering ModDimensions for " + MODID); }
